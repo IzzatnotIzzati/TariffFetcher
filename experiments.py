8953c4282e5a9b21f603e7112cd5e10832b18223
Izzat32 <redstonepro363@gmail.com>
2025-02-27 22:49:38 +0800

    Add functionality to extract and print tariff data in sen/kWh and kWh (TEST FOR REFERENCE)

--- experiments.py
+++ experiments.py
@@ -5,5 +5,12 @@ import requests
 
 response = requests.get('https://www.tnb.com.my/residential/pricing-tariffs')
 soup = bs(response.text, 'html.parser')
-tariff = soup.findAll('td', class_='content')
-print(tariff)
+
+# Dapatkan tarif dlm sen/kWh
+tariffCent = soup.find_all('td', class_='content', align='center', width='')
+
+# Tareik amnt kWh
+tariffkWh = soup.find_all('td', class_='content', valign='top')
+
+print(tariffkWh)
+print(tariffCent)
