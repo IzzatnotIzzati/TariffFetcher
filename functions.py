789b1d004ab1e67cd82747b2a2568ea889971a5f
IzzatnotIzzati <muhdizzat1117@gmail.com>
2025-03-01 11:52:54 +0800

    consolidated tariff scraper to different file in function for better readability 💅 penat dah baca code pening

--- functions.py
+++ functions.py
@@ -1,27 +1,15 @@
 from bs4 import BeautifulSoup as bs
 import requests
+import json
+from decimal import Decimal
 
 
 
-# Cari tariff TNB
-def tarriff():
-    url = 'https://www.tnb.com.my/residential/pricing-tariffs'
-    response = requests.get(url)
-    soup = bs(response.text, 'html.parser')
-    tariff = soup.find('td', class_='').text
-    print(tariff)
-    return float(tariff)
+
+
 
 def containsNum(inputString):
     result = any(char.isdigit() or char == '.' for char in inputString)
     #print(f"Checking '{inputString}': {result}")  # hurm masatu 57.10 x lepas so kna tambah ni
     return result
-    
 
-
-
-
-
-
-def calcBill(tariff, energyUsage):
-    return tariff * energyUsage
---
+++ tariffScraper.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup as bs
+import requests
+import json
+from decimal import Decimal
+from functions import containsNum
+
+
+
+# Cari tariff TNB
+def tarriff():
+    response = requests.get('https://www.tnb.com.my/residential/pricing-tariffs')
+    soup = bs(response.text, 'html.parser')
+
+    # Dapatkan tarif dlm sen/kWh
+    tariffCent = soup.find_all('td', class_='content', align='center', width='')
+
+    # Tareik amnt kWh
+    tariffkWh = soup.find_all('td', class_='content', valign='top')
+
+    # Convert jd JSON list
+    tariffPrices = []
+    kWhList = []
+    centList = []
+
+    for cent in tariffCent:
+        cent_text = cent.get_text(strip=True)
+        if containsNum(cent_text):
+            centList.append(cent_text)  # Store as string, not set
+
+    for kWh in tariffkWh:
+        kWh_text = kWh.get_text(strip=True)
+        if 'next' in kWh_text or 'first' in kWh_text:
+            kWhList.append(kWh_text)  # Store as string, not set
+
+    for kWh, cent in zip(kWhList, centList):
+        tariffPrices.append({
+            'kWh': kWh,
+            'cent': float(Decimal(cent) / Decimal('100')) # hurmm very janky la but it works ig
+    })
+
+    tariffPrices = json.dumps(tariffPrices, indent=4)
+
+    return tariffPrices
