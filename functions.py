b0a2a4ced2e3ba1869b48e0fc8d03c0db8af4756
IzzatnotIzzati <muhdizzat1117@gmail.com>
2025-03-01 11:13:17 +0800

    cleanup because reference is working :D

--- functions.py
+++ functions.py
@@ -14,7 +14,7 @@ def tarriff():
 
 def containsNum(inputString):
     result = any(char.isdigit() or char == '.' for char in inputString)
-    print(f"Checking '{inputString}': {result}")  # Debug print
+    #print(f"Checking '{inputString}': {result}")  # hurm masatu 57.10 x lepas so kna tambah ni
     return result
     
 
